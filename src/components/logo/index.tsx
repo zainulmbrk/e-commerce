8db6366691453e43866061db52a590aa6b417787
sangkanfaiq <sangkanfaiq@gmail.com>
2024-03-01 23:12:54 +0700

    feat: add logo

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -3,6 +3,6 @@ import React from "react";
 
 export const Logo: React.FC = () => {
   return (
-    <Image src="/icons/nextjs-icon.svg" alt="nextjs" width="96" height="58" />
+    <div className="text-slate-600 font-halimun text-2xl">Ethereal Enclave</div>
   );
 };
