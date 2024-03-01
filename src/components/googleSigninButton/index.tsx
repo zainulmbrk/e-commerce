2fcf41e758b6165bb30a10fde6fa1fa1b8e15701
sangkanfaiq <sangkanfaiq@gmail.com>
2024-03-01 23:13:47 +0700

    feat: create google login button

---
+++ src/components/googleSigninButton/index.tsx
@@ -0,0 +1,21 @@
+import { Button, Flex, Typography } from "antd";
+// import { signIn } from "next-auth/react";
+import React from "react";
+
+export const GoogleSigninButton = () => {
+	function handleLoginWithGoogle() {
+		// signIn("google", { callbackUrl: "/" });
+        console.log("Login google here")
+	}
+
+	return (
+		<Flex justify="center">
+			<Button onClick={handleLoginWithGoogle} className="h-10 w-9/12">
+				<Flex align="center">
+					<img src="/assets/images/google.png" style={{ width: 16, height: 16}} />
+					<div className="font-din-pro text-center w-full">Continue with Google</div>
+				</Flex>
+			</Button>
+		</Flex>
+	);
+};
