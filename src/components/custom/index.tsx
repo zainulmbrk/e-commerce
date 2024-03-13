37292e865b504b76fdac9ae8a766f3cec30288db
sangkanfaiq <sangkanfaiq@gmail.com>
2024-03-13 14:53:22 +0700

    feat: add custom render components

--- src/components/custom/index.tsx
+++ src/components/custom/index.tsx
@@ -1,11 +1,12 @@
 import { Flex } from "antd";
+import { Children } from "react";
 
-export const FormLoginLayout = ({children}: any) => {
+export const FormLoginLayout = ({ children }: any) => {
 	return (
 		<Flex justify="center">
-			<div className="w-9/12">
-				{children}
-			</div>
+			<div className="w-9/12">{children}</div>
 		</Flex>
 	);
 };
+
+export const EachItems = ({ render, data }: any) => <>{Children.toArray(data.map((item: any, index: any) => render(item, index)))}</>;
